5890535fe8104629cdf828ba4c041bfe1e47f01d
morenacaldarulo <morena.caldarulo@gmail.com>
2023-11-06 17:48:08 +0100

    create Servizio3Admin

---
+++ Servizio3Admin/src/main/java/it/gammainnovation/servizio3admin/Servizio3AdminApplication.java
@@ -0,0 +1,13 @@
+package it.gammainnovation.servizio3admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Servizio3AdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Servizio3AdminApplication.class, args);
+    }
+
+}
---
+++ Servizio3Admin/src/test/java/it/gammainnovation/servizio3admin/Servizio3AdminApplicationTests.java
@@ -0,0 +1,13 @@
+package it.gammainnovation.servizio3admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Servizio3AdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
