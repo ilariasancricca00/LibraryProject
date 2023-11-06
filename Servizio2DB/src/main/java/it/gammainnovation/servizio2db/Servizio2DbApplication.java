dc509bca71b89a86fb51f8793e9abf379263de32
frabarbara <francesco.barbara5697@gmail.com>
2023-11-06 16:16:14 +0100

    Create Servizio2: DB

---
+++ Servizio2DB/src/main/java/it/gammainnovation/servizio2db/Servizio2DbApplication.java
@@ -0,0 +1,13 @@
+package it.gammainnovation.servizio2db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Servizio2DbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Servizio2DbApplication.class, args);
+    }
+
+}
---
+++ Servizio2DB/src/test/java/it/gammainnovation/servizio2db/Servizio2DbApplicationTests.java
@@ -0,0 +1,13 @@
+package it.gammainnovation.servizio2db;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Servizio2DbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
