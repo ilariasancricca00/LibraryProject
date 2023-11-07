cbb012675477de9f12abcf5641df569922d0230a
morenacaldarulo <morena.caldarulo@gmail.com>
2023-11-07 16:25:56 +0100

    create Servizio4Restaurant

---
+++ Servizio4Restaurant/src/main/java/it/gammainnovation/servizio4restaurant/Servizio4RestaurantApplication.java
@@ -0,0 +1,13 @@
+package it.gammainnovation.servizio4restaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Servizio4RestaurantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Servizio4RestaurantApplication.class, args);
+    }
+
+}
---
+++ Servizio4Restaurant/src/test/java/it/gammainnovation/servizio4restaurant/Servizio4RestaurantApplicationTests.java
@@ -0,0 +1,13 @@
+package it.gammainnovation.servizio4restaurant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Servizio4RestaurantApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
